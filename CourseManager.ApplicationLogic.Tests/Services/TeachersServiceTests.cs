ecd0b49c5fc10bb329ffe897b4459074aa1619be
catalin-sbora <catalin.sbora@gmail.com>
2020-05-15 00:56:06 -0700

    add testinitialize

--- CourseManager.ApplicationLogic.Tests/Services/TeachersServiceTests.cs
+++ CourseManager.ApplicationLogic.Tests/Services/TeachersServiceTests.cs
@@ -14,59 +14,16 @@ namespace CourseManager.ApplicationLogic.Tests.Services
     [TestClass]
     public class TeachersServiceTests
     {
-        [TestMethod]
-        public void GetTeacherByUserId_ThrowsException_WhenUserIdHasInvalidValue()
-        {
-            //arrange
-            //ITeacherRepository teacherRepository, ICourseRepository
-            Mock<ITeacherRepository> teacherRepoMock = new Mock<ITeacherRepository>();
-            Mock<ICourseRepository> courseRepoMock = new Mock<ICourseRepository>();
-            TeachersService teacherService = new TeachersService(teacherRepoMock.Object, courseRepoMock.Object);
-            var badUserId = "jkajsdkasj dkj daksdj as";
-            //act            
-            //assert
-            Assert.ThrowsException<Exception>(() => {
-                teacherService.GetTeacherByUserId(badUserId);
-            });
-        }
+        private Mock<ITeacherRepository> teacherRepoMock = new Mock<ITeacherRepository>();
+        private Mock<ICourseRepository> courseRepoMock = new Mock<ICourseRepository>();
+        private Guid existingUserId = Guid.Parse("7299FFCC-435E-4A6D-99DF-57A4D6FBA712");
+        
 
-        [TestMethod]
-        public void GetTeacherByUserId_ThrowsEntityNotFound_WhenUserDoesNotExist()
-        {
-            Mock<ITeacherRepository> teacherRepoMock = new Mock<ITeacherRepository>();
-            Mock<ICourseRepository> courseRepoMock = new Mock<ICourseRepository>();
-            var nonExistingUserId = "7299FFCC-435E-4A6D-99DF-57A4D6FBA747";
-            var existingUserId = Guid.Parse("7299FFCC-435E-4A6D-99DF-57A4D6FBA712");
+        [TestInitialize]
+        public void InitializeTest()
+        {    
             
-            var teacher = new Teacher { 
-                Id = existingUserId,
-                Email="blabla@mail.com",
-                FirstName = "BlaBla",
-                LastName = "LastBlaBla",
-                PhoneNo = "0039947888474",
-                UserId = existingUserId
-            };
 
-            teacherRepoMock.Setup(teacherRepo => teacherRepo.GetTeacherByUserId(existingUserId))
-                            .Returns(teacher);                                                        
-            
-            TeachersService teacherService = new TeachersService(teacherRepoMock.Object, courseRepoMock.Object);            
-            
-            //act            
-            //assert
-            Assert.ThrowsException<EntityNotFoundException>(() => {
-                teacherService.GetTeacherByUserId(nonExistingUserId);
-            });
-        }
-
-        [TestMethod]
-        public void GetTeacherByUserId_Returns_UserWhenExists()
-        {
-            Mock<ITeacherRepository> teacherRepoMock = new Mock<ITeacherRepository>();
-            Mock<ICourseRepository> courseRepoMock = new Mock<ICourseRepository>();
-            var nonExistingUserId = "7299FFCC-435E-4A6D-99DF-57A4D6FBA747";
-            var existingUserId = Guid.Parse("7299FFCC-435E-4A6D-99DF-57A4D6FBA712");
-            Exception throwException = null;
             var teacher = new Teacher
             {
                 Id = existingUserId,
@@ -79,8 +36,44 @@ namespace CourseManager.ApplicationLogic.Tests.Services
 
             teacherRepoMock.Setup(teacherRepo => teacherRepo.GetTeacherByUserId(existingUserId))
                             .Returns(teacher);
+            
+        }
 
-            TeachersService teacherService = new TeachersService(teacherRepoMock.Object, courseRepoMock.Object);
+        [TestMethod]
+        public void GetTeacherByUserId_ThrowsException_WhenUserIdHasInvalidValue()
+        {
+            //arrange
+            //ITeacherRepository teacherRepository, ICourseRepository
+            var teacherService = new TeachersService(teacherRepoMock.Object, courseRepoMock.Object);
+
+            var badUserId = "jkajsdkasj dkj daksdj as";
+            //act            
+            //assert
+            Assert.ThrowsException<Exception>(() => {
+                teacherService.GetTeacherByUserId(badUserId);
+            });
+        }
+
+        [TestMethod]
+        public void GetTeacherByUserId_ThrowsEntityNotFound_WhenUserDoesNotExist()
+        {
+            //arrange
+            var nonExistingUserId = "7299FFCC-435E-4A6D-99DF-57A4D6FBA747";                                                   
+            var teacherService = new TeachersService(teacherRepoMock.Object, courseRepoMock.Object);            
+            
+            //act            
+            //assert
+            Assert.ThrowsException<EntityNotFoundException>(() => {
+                teacherService.GetTeacherByUserId(nonExistingUserId);
+            });
+        }
+
+        [TestMethod]
+        public void GetTeacherByUserId_Returns_UserWhenExists()
+        {
+           
+            Exception throwException = null;
+            var teacherService = new TeachersService(teacherRepoMock.Object, courseRepoMock.Object);
             Teacher user = null; 
             //act   
             try
